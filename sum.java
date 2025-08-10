86b5cddd4491fdb67d15a3a5034184ae4355be9f
rishav <rishav0043@gmail.com>
2025-08-11 01:20:54 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -3,6 +3,7 @@ class Sum
     public static void main(String args[])
     {
         System.out.println("hello this is sum function");
-        System.out.println("these are new changes i have just now")
+        System.out.println("these are new changes i have just now");
+        System.out.println("again a few changes");
     }
 }
